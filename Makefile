093b3e9670aae0186c9b00b4b557550eb8c7482f
Vahe Nazaryan <49231700+23nazaryan@users.noreply.github.com>
2024-01-17 06:01:38 -0800

    implement swagger (#12)
    * implement swagger
    
    * implement swagger (fix)

--- Makefile
+++ Makefile
@@ -42,4 +42,17 @@ server:
 mock:
 	mockgen -package mockdb -destination db/mock/store.go github.com/23nazaryan/simplebank/db/sqlc Store
 
-.PHONY: network postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 db_docs db_schema sqlc test server mock
+proto:
+	rm -f pb/*.go
+	rm -f doc/swagger/*.swagger.json
+	protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative \
+		  --go-grpc_out=pb --go-grpc_opt=paths=source_relative \
+		  --grpc-gateway_out=pb --grpc-gateway_opt=paths=source_relative \
+		  --openapiv2_out=doc/swagger --openapiv2_opt=allow_merge=true,merge_file_name=simplebank\
+		  proto/*.proto
+		  statik -src=./doc/swagger -dest=./doc
+
+evans:
+	evans --host localhost --port 4000 -r repl
+
+.PHONY: network postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 db_docs db_schema sqlc test server mock proto evans
--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"github.com/23nazaryan/simplebank/api"
 	db "github.com/23nazaryan/simplebank/db/sqlc"
+	_ "github.com/23nazaryan/simplebank/doc/statik"
+	"github.com/23nazaryan/simplebank/gapi"
+	pb "github.com/23nazaryan/simplebank/pb"
 	"github.com/23nazaryan/simplebank/util"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
+	"net"
+	"net/http"
 )
 
 func main() {
@@ -21,11 +32,85 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcMux := runtime.NewServeMux(
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+			MarshalOptions: protojson.MarshalOptions{
+				UseProtoNames: true,
+			},
+			UnmarshalOptions: protojson.UnmarshalOptions{
+				DiscardUnknown: true,
+			},
+		}))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot create handler server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandlew := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandlew)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener:", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	err = http.Serve(listener, mux)
+
+	if err != nil {
+		log.Fatal("cannot start HTTP server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server:", err)
 	}
-	err = server.Start(config.ServerAddress)
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
